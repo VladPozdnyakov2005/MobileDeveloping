98518dec17914b8166c21500e3b46fec0c8b885b
VladPozdnyakov2005 <vlpozdnyak250305@mail.ru>
2024-11-12 00:11:39 +0300

    Project 1 video 12th

--- app/src/main/java/com/example/mobiledeveloping/MainActivity.kt
+++ app/src/main/java/com/example/mobiledeveloping/MainActivity.kt
@@ -1,47 +1,138 @@
 package com.example.mobiledeveloping
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.example.mobiledeveloping.data.WeatherModel
 import com.example.mobiledeveloping.ui.theme.MobileDevelopingTheme
+import com.example.mobiledeveloping.ui_components.DialogSearch
+import com.example.mobiledeveloping.ui_components.MainScreen
+import com.example.mobiledeveloping.ui_components.TabLayout
+import org.json.JSONObject
 
+
+const val API_KEY = "3f38dcd2a0d6496a98b61205240911"
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             MobileDevelopingTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                val daysList = remember{
+                    mutableStateOf(listOf<WeatherModel>())
+                }
+                val dialogState = remember{
+                    mutableStateOf(false)
+                }
+
+                val currentDay = remember{
+                    mutableStateOf(WeatherModel(
+                        "",
+                        "",
+                        "0.0",
+                        "",
+                        "",
+                        "0.0",
+                        "0.0",
+                        ""
                     )
+                    )
+                }
+                if(dialogState.value){
+                    DialogSearch(dialogState, onSubmit = {
+                        getData(it, this, daysList, currentDay)
+                    })
+                }
+                getData("London", this, daysList, currentDay)
+                Image(
+                    painter  = painterResource(
+                        id = R.drawable.weather_bg
+                    ),
+                    contentDescription = "im1",
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .alpha(0.5f),
+                    contentScale = ContentScale.FillBounds
+                )
+                Column{
+                    MainScreen(currentDay, onClickSync = {
+                        getData("London", this@MainActivity, daysList, currentDay)
+                    }, onClickSearch = {
+                        dialogState.value = true
+                    }
+                    )
+                    TabLayout(daysList, currentDay)
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+private fun getData(city:String, context: Context,
+                    daysList: MutableState<List<WeatherModel>>,
+                    currentDay: MutableState<WeatherModel>){
+    val url = "http://api.weatherapi.com/v1/forecast.json?key=$API_KEY" +
+            "&q=$city" +
+            "&days=" +
+            "3" +
+            "&aqi=no&alerts=no"
+    val queue = Volley.newRequestQueue(context)
+    val sRequest = StringRequest(
+        Request.Method.GET,
+        url,{
+                response ->
+            val list = getWeatherByDays(response)
+            currentDay.value = list[0]
+            daysList.value = list
+        },
+        {
+            Log.d("MyLog", "VolleyError: $it")
+        }
     )
+    queue.add(sRequest)
+
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MobileDevelopingTheme {
-        Greeting("Android")
+private fun getWeatherByDays(response: String): List<WeatherModel>{
+    if(response.isEmpty()) return listOf()
+    val list = ArrayList<WeatherModel>()
+    val mainObject = JSONObject(response)
+    val city = mainObject.getJSONObject("location").getString("name")
+    val days = mainObject.getJSONObject("forecast").getJSONArray("forecastday")
+
+    for (i in 0 until days.length()){
+        val item = days[i] as JSONObject
+        list.add(
+            WeatherModel(
+                city,
+                item.getString("date"),
+                "",
+                item.getJSONObject("day").getJSONObject("condition").getString("text"),
+                item.getJSONObject("day").getJSONObject("condition").getString("icon"),
+                item.getJSONObject("day").getString("maxtemp_c"),
+                item.getJSONObject("day").getString("condition"),
+                item.getJSONArray("hour").toString()
+            )
+        )
     }
+    list[0] = list[0].copy(
+        time = mainObject.getJSONObject("current").getString("last_updated"),
+        currentTemp = mainObject.getJSONObject("current").getString("temp_c"),
+
+        )
+    return list
 }
